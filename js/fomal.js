9c72ddcc57c0fb3a9f871c2a2e19f1dbd091e374
lxk2070 <lxk2070@163.com>
2025-07-29 10:53:09 +0800

    Site updated: 2025-07-29 10:53:01

--- js/fomal.js
+++ js/fomal.js
@@ -1325,7 +1325,7 @@ document.addEventListener('visibilitychange', function () {
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖就知道你会回来～';
+    document.title = '就知道你会回来～😁';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
@@ -2824,56 +2824,57 @@ setInterval(() => {
 
 /* fps检测 start */
 // if (window.localStorage.getItem("fpson") == undefined || window.localStorage.getItem("fpson") == "1") {
-//   var rAF = function () {
-//     return (
-//       window.requestAnimationFrame ||
-//       window.webkitRequestAnimationFrame ||
-//       function (callback) {
-//         window.setTimeout(callback, 1000 / 60);
-//       }
-//     );
-//   }();
-//   var frame = 0;
-//   var allFrameCount = 0;
-//   var lastTime = Date.now();
-//   var lastFameTime = Date.now();
-//   var loop = function () {
-//     var now = Date.now();
-//     var fs = (now - lastFameTime);
-//     var fps = Math.round(1000 / fs);
+if (window.localStorage.getItem("fpson") == "1") {
+  var rAF = function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
+  }();
+  var frame = 0;
+  var allFrameCount = 0;
+  var lastTime = Date.now();
+  var lastFameTime = Date.now();
+  var loop = function () {
+    var now = Date.now();
+    var fs = (now - lastFameTime);
+    var fps = Math.round(1000 / fs);
 
-//     lastFameTime = now;
-//     // 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
-//     allFrameCount++;
-//     frame++;
+    lastFameTime = now;
+    // 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
+    allFrameCount++;
+    frame++;
 
-//     if (now > 1000 + lastTime) {
-//       var fps = Math.round((frame * 1000) / (now - lastTime));
-//       if (fps <= 5) {
-//         var kd = `<span style="color:#bd0000">卡成ppt🤢</span>`
-//       } else if (fps <= 15) {
-//         var kd = `<span style="color:red">电竞级帧率😖</span>`
-//       } else if (fps <= 25) {
-//         var kd = `<span style="color:orange">有点难受😨</span>`
-//       } else if (fps < 35) {
-//         var kd = `<span style="color:#9338e6">不太流畅🙄</span>`
-//       } else if (fps <= 45) {
-//         var kd = `<span style="color:#08b7e4">还不错哦😁</span>`
-//       } else {
-//         var kd = `<span style="color:#39c5bb">十分流畅🤣</span>`
-//       }
-//       document.getElementById("fps").innerHTML = `FPS:${fps} ${kd}`;
-//       frame = 0;
-//       lastTime = now;
-//     };
+    if (now > 1000 + lastTime) {
+      var fps = Math.round((frame * 1000) / (now - lastTime));
+      if (fps <= 5) {
+        var kd = `<span style="color:#bd0000">卡成ppt🤢</span>`
+      } else if (fps <= 15) {
+        var kd = `<span style="color:red">电竞级帧率😖</span>`
+      } else if (fps <= 25) {
+        var kd = `<span style="color:orange">有点难受😨</span>`
+      } else if (fps < 35) {
+        var kd = `<span style="color:#9338e6">不太流畅🙄</span>`
+      } else if (fps <= 45) {
+        var kd = `<span style="color:#08b7e4">还不错哦😁</span>`
+      } else {
+        var kd = `<span style="color:#39c5bb">十分流畅🤣</span>`
+      }
+      document.getElementById("fps").innerHTML = `FPS:${fps} ${kd}`;
+      frame = 0;
+      lastTime = now;
+    };
 
-//     rAF(loop);
-//   }
+    rAF(loop);
+  }
 
-//   loop();
-// } else {
-//   document.getElementById("fps").style = "display:none!important"
-// }
+  loop();
+} else {
+  document.getElementById("fps").style = "display:none!important"
+}
 /* fps检测 end */
 
 //----------------------------------------------------------------
@@ -2924,7 +2925,7 @@ function clearItem() {
 
 // 设置字体
 if (localStorage.getItem("font") == undefined) {
-  localStorage.setItem("font", "LXGW");
+  localStorage.setItem("font", "YSHST");
 }
 setFont(localStorage.getItem("font"));
 function setFont(n) {
@@ -3005,7 +3006,7 @@ function setSnow() {
 
 // 帧率监测开关
 if (localStorage.getItem("fpson") == undefined) {
-  localStorage.setItem("fpson", "1");
+  localStorage.setItem("fpson", "0");
 }
 function fpssw() {
   if (document.getElementById("fpson").checked) {
@@ -3440,10 +3441,10 @@ function createWinbox() {
   } else if (localStorage.getItem("universe") == "none") {
     document.getElementById("universeSet").checked = false;
   }
-  if (localStorage.getItem("fpson") == "1") {
-    document.getElementById("fpson").checked = true;
-  } else {
+  if (localStorage.getItem("fpson") == "0") {
     document.getElementById("fpson").checked = false;
+  } else {
+    document.getElementById("fpson").checked = true;
   }
   if (localStorage.getItem("rs") == "block") {
     document.getElementById("rightSideSet").checked = true;
